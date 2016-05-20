dde6ca4c374e8318be5b955a5953649451680283
stern15 <ssemasuka@gmail.com>
2016-05-20 13:19:11 +0300

    linking a function to an object through a method in js

---
+++ js understand the weird parts/section 2/app14.js
@@ -0,0 +1,13 @@
+var playerOne = { name:"stern", age:22, gender:"male", score:1000000 };
+
+var playerTwo = { name:"yvette", age:27, gender:"female", score:50000 };
+
+function playerDetails(){
+   console.log( this.name + " with " + this.age + " years old has " +this.score + " points " );
+}
+
+playerOne.showDetails = playerDetails;
+
+playerTwo.showDetails = playerDetails;
+
+playerOne.showDetails();
