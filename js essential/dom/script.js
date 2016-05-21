e826be667fcbef6791ba9a3358f49ab7f559cdf1
stern15 <ssemasuka@gmail.com>
2016-05-21 15:52:46 +0300

    changing the dom

---
+++ js essential/dom/script.js
@@ -0,0 +1,8 @@
+var mainContent = document.getElementById("mainContent");
+mainContent.setAttribute("align","right");
+
+var mainTitle = document.getElementById("mainTitle");
+console.log(mainTitle.innerHTML);
+
+var sidebar = document.getElementById("sidebar");
+console.log(sidebar.innerHTML);
