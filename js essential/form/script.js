25b88e2d35c594a12c118cc87c75522cbad37627
stern15 <ssemasuka@gmail.com>
2016-05-26 22:02:40 +0300

    eventhadler with the submit button

---
+++ js essential/form/script.js
@@ -0,0 +1,20 @@
+var errorVal = document.getElementById("errorMessage");
+var emailVal = document.getElementById("email").value;
+var form = document.getElementById("frmContact");
+
+function submitFormHandler(){
+    form.onsubmit = function(){
+    if (if emailVal === ""){
+        errorVal.innerHTML = "Please provide at least an email";
+        return false;
+    }
+    else{
+        errorVal.innerHTML = "";
+        return true;
+    }
+    };
+}
+
+window.onload = function (){
+    submitFormHandler();
+};
