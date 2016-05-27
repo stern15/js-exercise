fe1980368f0ba456718d65b9f35715249c5326a9
stern15 <ssemasuka@gmail.com>
2016-05-27 12:47:04 +0300

    hidding a form with onclick event and eventhandler

---
+++ js essential/hide and show form/script.js
@@ -0,0 +1,17 @@
+
+
+function showPage(){
+    document.getElementById("tourSelection").style.display = "none";
+    document.getElementById("brochures").onclick= function(){
+        if(document.getElementById("brochures").checked){
+            document.getElementById("tourSelection").style.display = "block";
+        }
+        else {
+            document.getElementById("tourSelection").style.display = "none";
+        }
+    };
+}
+
+window.onload = function(){
+  showPage();
+};
