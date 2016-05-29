b6f8c2535a1036dfc61eef21cbc5734671b81cd6
stern15 <ssemasuka@gmail.com>
2016-05-29 22:05:39 +0300

    strict mode

---
+++ js essential/strict mode/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// didn't use var
+var foo = "Hello world";
+
+// new object with two properties with the same name
+var myObject = { samename: 1, diffname: 2 };
+
+// function with the same name for multiple parameters
+function myFunction( a,b,c) {
+   return a + b + c;
+}
+
+document.getElementById("mainContent").innerHTML = foo;
+
+
+
