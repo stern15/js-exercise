cdb52b39466bfb064d1c6d5606520df50c38ad4f
stern15 <ssemasuka@gmail.com>
2016-05-29 17:18:08 +0300

    playing with jquery

---
+++ js essential/jquery/script.js
@@ -0,0 +1,35 @@
+//$("#mainArticle").addClass("highlight");
+
+//$(".tourDescription").addClass("highlight");
+
+//$("li").addClass("highlight");
+
+//$("li:first").addClass("highlight");
+
+//$("p:contains('kids')").addClass("highlight");
+
+//$("p").fadeOut(4000);
+
+//$("h2").click(function(){
+//    $("p").show(4000);
+//});
+
+//$("#pageID").click(function(){
+//    $("#pageID").text("you clicked me!");
+//});
+
+//$("h2"//).click(function(){
+//    $(this).text("you clicked me!");
+//});
+
+//$("p").click(function(){
+//    $(this).fadeOut(4000);
+//});
+
+$("document").ready(function(){
+    $("#pageID").text("clicked already!!");
+});
+
+$("document").ready(function(){
+    $("h1").css("color","red");
+});
