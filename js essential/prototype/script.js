40510a3abbf9f2d55a809b2fbb9c40cd6f7e18d9
stern15 <ssemasuka@gmail.com>
2016-05-31 18:02:08 +0300

    the prototype demostred with an object with proprities

--- js essential/prototype/script.js
+++ js essential/prototype/script.js
@@ -1,4 +1,29 @@
+function Player(n,s,r){
+    this.name = n;
+    this.score = s;
+    this.rank = r;
 
+}
+
+Player.prototype.logInfo = function(){
+    console.log("I am: " + this.name);
+};
+
+Player.prototype.promoted = function(){
+    this.rank++;
+    console.log("the rank now is: " + this.rank);
+};
+
+var fred = new Player("fred",2000,2);
+
+fred.promoted();
+fred.logInfo();
+
+
+var bob = new Player("bob",300,9);
+
+bob.logInfo();
+bob.promoted();
 
 
 
