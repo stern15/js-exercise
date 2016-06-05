789410bf838f9dbb0338fba2c851d391934f7294
stern15 <ssemasuka@gmail.com>
2016-06-05 12:34:02 +0300

    response handler in ajax and the get and send request

--- js essential/ajax/script.js
+++ js essential/ajax/script.js
@@ -1,21 +1,14 @@
-// Simple Ajax example.
+var myReq;
 
-// 1: Create the request 
-var myRequest;
-
-// feature check!
-if (window.XMLHttpRequest) {  // does it exist? we're in Firefox, Safari etc.
-    myRequest = new XMLHttpRequest();
-} else if (window.ActiveXObject) { // if not, we're in IE
-    myRequest = new ActiveXObject("Microsoft.XMLHTTP");
+if(window.XMLHttpRequest){
+    myReq = new XMLHttpRequest();
 }
-
-// 2: create an event handler for our request to call back
-myRequest.onreadystatechange = function(){
-    console.log("We were called!");
+else if(window.ActiveXObject){
+    myReq = new ActiveXObject("Microsoft.XMLHTTP");
+}
+myReq.onreadystatechange = function(){
+    console.log("we are called");
 };
 
-// open and send it
-myRequest.open('GET', 'simple.txt', true);
-// any parameters?
-myRequest.send(null);
+myReq.open("GET","simple.txt",true);
+myReq.send(null);
