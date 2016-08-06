22a308177e9987a785ce18d5e20f19c39a931cfd
stern15 <ssemasuka@gmail.com>
2016-08-06 14:53:05 +0300

    how to access to object propreties using []

---
+++ js understand the weird parts/section 4/app9.js
@@ -0,0 +1,10 @@
+var person = new Object();
+
+person["firstName"]="tonny";
+person["lastName"]="semasuka";
+
+
+var firstNameProp = "firstName";
+
+console.log(person);
+console.log(person[firstNameProp]);
