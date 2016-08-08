ff89022d3c01df0da329972e9eb4f17cef59b070
stern15 <ssemasuka@gmail.com>
2016-08-08 13:24:21 +0300

    namespace in js

---
+++ js understand the weird parts/section 4/app3.js
@@ -0,0 +1,17 @@
+var greet = "hello";
+var greet = "hola";
+
+console.log(greet);
+
+var english = {
+    greetings:{
+        basic:"hello"
+    }
+};
+var spanish = {};
+
+
+spanish.greet = "hola";
+
+console.log(english);
+console.log(spanish);
