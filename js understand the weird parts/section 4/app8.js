6bef84279cded40170662bc272f9a233e722e914
stern15 <ssemasuka@gmail.com>
2016-08-10 12:33:47 +0300

    the = operator which makes an exception by reference

--- js understand the weird parts/section 4/app8.js
+++ js understand the weird parts/section 4/app8.js
@@ -2,7 +2,30 @@ var c = {greeting:"hi"};
 var d;
 
 d = c;
-c.greeting = "hello";
+c.greeting = "hello"; //mutation
+
+console.log(c);
+console.log(d);
+
+function changeGreetings(obj){
+    obj.greeting = "holla";
+}
+changeGreetings(d);
+
+console.log(c);
+console.log(d);
+
+c.greeting = "bite";
+
+console.log(c);
+console.log(d);
+
+c = {greeting:"salama"};
+
+console.log(c);
+console.log(d);
+
+d = c;
 
 console.log(c);
 console.log(d);
