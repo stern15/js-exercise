1b223c00e230eb922b814af1eeba1fe94350482b
stern15 <ssemasuka@gmail.com>
2016-08-11 18:37:25 +0300

    arguement in js

---
+++ js understand the weird parts/section 4/app12.js
@@ -0,0 +1,10 @@
+function greet (firstName,lastName,language){
+    console.log(firstName);
+    console.log(lastName);
+    console.log(language);
+    console.log("----------------");
+}
+greet ();
+greet ("stern");
+greet ("stern","semasuka");
+greet ("stern","semasuka","ki");
