faecba8222f5da67915f4078ca3aa3d7e04b6fdd
stern15 <ssemasuka@gmail.com>
2016-08-11 19:54:43 +0300

    array-like structure arguements

--- js understand the weird parts/section 4/app12.js
+++ js understand the weird parts/section 4/app12.js
@@ -1,8 +1,17 @@
 function greet (firstName,lastName,language){
     language = language || "en";
+
+    if(arguments.length === 0){
+        console.log("missing parameter");
+        console.log("----------------");
+        return;
+    }
+
     console.log(firstName);
     console.log(lastName);
     console.log(language);
+    console.log(arguments);
+    console.log("the arguments 0 is: " + arguments[1]);
     console.log("----------------");
 }
 greet ();
