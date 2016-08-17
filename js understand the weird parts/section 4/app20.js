aefbe44beb1b99ebff2da8a60ebee892d30631ed
stern15 <ssemasuka@gmail.com>
2016-08-17 19:42:35 +0300

    let as a var

--- js understand the weird parts/section 4/app20.js
+++ js understand the weird parts/section 4/app20.js
@@ -14,3 +14,40 @@ fs[0]();
 fs[1]();
 fs[2]();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function buildFunction2(){
+    var arr=[];
+    for (var i = 0; i < 3 ;i++ ){
+        let j = i;
+        arr.push(
+            function(){
+                console.log(j);
+            }
+        );
+    }
+    return arr;
+}
+var fs2 = buildFunction2();
+fs2[0]();
+fs2[1]();
+fs2[2]();
+
