b259423d0bc539960766f9cdc8d92584ea6c0c48
stern15 <ssemasuka@gmail.com>
2016-08-19 22:20:00 +0300

    function currying in js

--- js understand the weird parts/section 4/app26.js
+++ js understand the weird parts/section 4/app26.js
@@ -36,6 +36,17 @@ var person2={
 console.log(person.getFullName.apply(person2));
 
 
+//function curring
+
+function multiply(a,b){
+    return a*b;
+}
+var multiplyByTwo = multiply.bind(this,2);
+console.log(multiplyByTwo(5));
+
+
+var multiplyByThree = multiply.bind(this,3);
+console.log(multiplyByThree(9));
 
 
 
