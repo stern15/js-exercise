611f8236cc632bbe7f2e99ed3e52411adbd2589f
stern15 <ssemasuka@gmail.com>
2016-08-19 22:19:32 +0300

    function borrowing in js

---
+++ js understand the weird parts/section 4/app26.js
@@ -0,0 +1,45 @@
+var person = {
+    firstName:"semasuka",
+    lastName:"stern",
+    getFullName:function(){
+        var fullname= this.firstName + " " + this.lastName;
+        return fullname;
+    }
+};
+
+var logName = function (lang1,lang2){
+    console.log("logged: "+ this.getFullName());
+    console.log("arguments: "+ lang1 +" "+ lang2);
+    console.log("-------------------");
+};
+
+var logPersonName = logName.bind(person);
+logPersonName("en");
+
+
+logName.call(person,"es");
+logName.apply(person,["en","es"]);
+
+(function (lang1,lang2){
+    console.log("logged: "+ this.getFullName());
+    console.log("arguments: "+ lang1 +" "+ lang2);
+    console.log("-------------------");
+}.apply(person,["en","it"]));
+
+
+//function borrowing
+
+var person2={
+    firstName:"Yvette",
+    lastName:"Sinarinzi"
+};
+console.log(person.getFullName.apply(person2));
+
+
+
+
+
+
+
+
+
